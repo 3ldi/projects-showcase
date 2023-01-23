9c86e9bbe694f81fcf092a4d735ff9ad837b80c8
eldi <66099058+3ldi@users.noreply.github.com>
2023-01-22 21:57:37 -0500

    Update settings.py

--- portfolio_project/portfolio_project/settings.py
+++ portfolio_project/portfolio_project/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3ae6dy%rlme66xqux!hi_^l(0teqf1i6am^)!1=%-4h5o@jolm'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
